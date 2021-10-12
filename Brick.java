66dafd475eb5734397eb0487acdd73028afd9538
Bel1eveme <alex180103@gmail.com>
2021-10-12 21:46:11 +0300

    all inits

--- Brick.java
+++ Brick.java
@@ -2,8 +2,18 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Brick {
-    int brickXPos, brickYPos; // объявляем переменные координат позиции блока.
-    int brickWidth = 80; // ширина блока
-    int brickHeight = 40; // высота блока
+
+    int brickXPos, brickYPos; // переменные координат позиции блока.
+
+    int brickWidth = 80; // ширина блока.
+    int brickHeight = 40; // высота блока.
+
+    int xOffset = 150; // отступ справа.
+    int yOffset = 50; // отступ сверху.
+
+    int xGap = 10; // промежуток по горизонтали.
+    int yGap = 10; // промежуток по вертикали.
+
+
 }
 
