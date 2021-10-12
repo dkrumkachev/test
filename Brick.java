a0ea3ca767af8168db1c6b3c011b6f0c88e63b9a
Bel1eveme <alex180103@gmail.com>
2021-10-12 21:34:01 +0300

    X Y pos

--- Brick.java
+++ Brick.java
@@ -1,6 +1,7 @@
 
 
 public class Brick {
-    int brickXPos, brickYPos; // объявляем переменные позиции блока.
-
+    int brickXPos, brickYPos; // объявляем переменные координат позиции блока.
+    int brickWidth = 80; // ширина блока
+    int brickHeight = 40; //
 }
