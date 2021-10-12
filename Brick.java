7154da53a2c3bac82cbff0c9c99fafaeeba52d42
Bel1eveme <alex180103@gmail.com>
2021-10-12 21:35:06 +0300

    width+height

--- Brick.java
+++ Brick.java
@@ -3,5 +3,6 @@
 public class Brick {
     int brickXPos, brickYPos; // объявляем переменные координат позиции блока.
     int brickWidth = 80; // ширина блока
-    int brickHeight = 40; //
+    int brickHeight = 40; // высота блока
 }
+
